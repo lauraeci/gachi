41cd1a9b8044c921e064887cf8459434bc3ffa7e
laureci <lmrshall@gmail.com>
2018-12-28 22:14:15 -0800

    Active Storage models

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_25_213754) do
+ActiveRecord::Schema.define(version: 2018_12_29_061240) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name"
-    t.integer "record_id"
-    t.string "record_type"
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "filename"
+    t.string "key", null: false
+    t.string "filename", null: false
     t.string "content_type"
     t.text "metadata"
-    t.text "checksum"
-    t.integer "byte_size"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "auction_items", force: :cascade do |t|
@@ -132,7 +139,7 @@ ActiveRecord::Schema.define(version: 2018_12_25_213754) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "lootSpecs", force: :cascade do |t|
+  create_table "loot_specs", force: :cascade do |t|
     t.decimal "rarity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -147,7 +154,7 @@ ActiveRecord::Schema.define(version: 2018_12_25_213754) do
     t.string "name"
   end
 
-  create_table "loot_specs", force: :cascade do |t|
+  create_table "loot_types", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
